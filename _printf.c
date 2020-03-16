d1cc57c8f2cf191555d8b8f2bd01298778219bfe
luismvargasg <1610@holbertonschool.com>
2020-03-16 17:39:44 +0000

    adding _printf function

---
+++ _printf.c
@@ -0,0 +1,14 @@
+#include "holberton.h"
+
+/**
+ * _printf - function that produces output according to a format.
+ * @format: list of types of arguments passed to the function.
+ *
+ * Return: the number of characters printed excluding the null byte.
+ */
+
+int _printf(const char *format, ...)
+{
+
+
+}
